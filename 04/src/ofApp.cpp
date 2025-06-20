15b276e1535b2fbe0de9430e654f4964b366c78c
jaypowers <japow75@gmail.com>
2025-06-20 13:06:43 -0400

    Had to switch to another project, 3 is working, not right, 4 is now new.

--- 04/src/ofApp.cpp
+++ 04/src/ofApp.cpp
@@ -1,42 +1,18 @@
 #include "ofApp.h"
-#include "spdlog/spdlog.h"
-#include <cmath>
-
-double y = 0.0; // Initialize y for sine wave
-double x = 0.0; // Initialize x for cosine wave
-// 2
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
-	ofSetWindowTitle("Genuary 2025 - 03");
-	ofBackground(0, 0, 0);
-	ofSetFrameRate(3);
-	ofSetCircleResolution(100); 
-	ofEnableAntiAliasing(); 
-	ofTranslate(-ofGetWidth() / 2.0, -ofGetHeight() / 2.0); // Center the drawing
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+
 }
 
-// Fix for lnt-integer-float-division and E0439 errors
-void ofApp::draw() {
+//--------------------------------------------------------------
+void ofApp::draw(){
 
-	double dsin = ofRandom(ofGetWidth()/2);
-	double dcos = ofRandom(ofGetHeight()/2);
-
-	spdlog::info("dcos {}!", dcos);
-	spdlog::info("dsin {}!", dsin);
-
-    ofSetColor(ofRandom(0, 255), ofRandom(0, 255), ofRandom(0, 255)); // Set color to red
-    ofDrawCircle(ofGetWidth() / 2.0 + dcos, ofGetHeight() / 2.0 + dsin, 50);
-	
-
-	// 14
-	// 23 total
 }
 
 //--------------------------------------------------------------
