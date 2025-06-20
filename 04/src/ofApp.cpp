c2d9745ba8333766d42f4bce3dcaab7605fd25c3
jaypowers <japow75@gmail.com>
2025-06-20 13:29:36 -0400

    Finished with 04 - Black on Black

--- 04/src/ofApp.cpp
+++ 04/src/ofApp.cpp
@@ -1,18 +1,27 @@
 #include "ofApp.h"
 
+#define WIDTH 1024
+#define HEIGHT 768
+
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+	ofSetWindowTitle("Genuary 2025 - 04");
+	ofBackground(255);
+	ofSetFrameRate(2);
+	ofBlendMode(OF_BLENDMODE_ADD);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+	ofSetColor(0,0,0, 240);
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+	ofPushMatrix();
+		ofDrawRectangle(ofRandom(WIDTH), ofRandom(HEIGHT), ofRandom(WIDTH), ofRandom(HEIGHT));
+		ofDrawRectangle(ofRandom(WIDTH), ofRandom(HEIGHT), ofRandom(WIDTH), ofRandom(HEIGHT));
+	ofPopMatrix();
 }
 
 //--------------------------------------------------------------
